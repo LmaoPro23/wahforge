dfd6e3ea972d0e143be13dc02ecbdfd4badb253f
LmaoPro23 <132865621+LmaoPro23@users.noreply.github.com>
2024-12-12 12:15:46 +0530

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--pin-shared-memory --cuda-stream --cuda-malloc --xformers"
 
 # python3 executable
 #python_cmd="python3"
